4e8d34b5d0445f7cec52ab8af6d4f436a24f90d5
DevFlamez <samuelakinwole@gmail.com>
2024-02-08 15:31:56 +0100

    Add project files.

---
+++ TodoAppBlazorServer/Service/ITodoService.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoAppBlazorServer.Service
+{
+    public interface ITodoService
+    {
+        void Add(TodoItem item);
+        IEnumerable<TodoItem> GetAll();
+        void Delete(TodoItem item);
+        void Complete(TodoItem item);
+        void UnCompleted(TodoItem item);
+    }
+}
---
+++ TodoAppBlazorServer/Service/TodoService.cs
@@ -0,0 +1,38 @@
+﻿namespace TodoAppBlazorServer.Service
+{
+    public class TodoService : ITodoService
+    {
+        private readonly IList<TodoItem> _todoItems;
+        public TodoService()
+        {
+            _todoItems = new List<TodoItem>
+            {
+                new TodoItem ("Wash clothes"),
+                new TodoItem ("Sweep"),
+                new TodoItem ("Eat Fruits")
+            };
+        }
+        public void Add(TodoItem item)
+        {
+            _todoItems.Add(item);
+        }
+
+        public IEnumerable<TodoItem> GetAll()
+        {
+            return _todoItems.ToList();
+        }
+
+        public void Delete(TodoItem item)
+        {
+            _todoItems.Remove(item);
+        }
+        public void Complete(TodoItem item)
+        {
+            item.Completed = true;
+        }
+        public void UnCompleted(TodoItem item)
+        {
+            item.Completed = false;
+        }
+    }
+}
